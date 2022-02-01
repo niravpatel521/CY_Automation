1143f72b0ffb9e032e84ebef3931eda7c61c45a3
Nirav Patel <niravpatel521@gmail.com>
2022-02-01 16:55:26 +0530

    scrip added for JUnit

--- cypress/integration/tests/test_spec.js
+++ cypress/integration/tests/test_spec.js
@@ -7,6 +7,6 @@ describe('My First Test', function() {
     it('Does not do much too!', function() {
       cy.visit('/');
       expect(true).to.equal(false)
-      this.test.attachments = ['/cypress/screenshots/one.png'];
+      this.test.attachments = ['/cypress/capture/one.png'];
     })
   })
